e96dda7e9fc771a7d51c4aacd1df0f58a90525ac
Sarpong <esarpong51@gmail.com>
2020-11-07 07:42:49 +0000

    Initial commit

---
+++ src/main/java/com/outrespace/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.outrespace.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
